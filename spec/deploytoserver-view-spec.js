3cde4aca6a18707fb8fdae3ce8a5f1df1fe2be66
reesebo <qzb2002e@163.com>
2016-05-23 13:11:34 +0800

    init

---
+++ spec/deploytoserver-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DeploytoserverView from '../lib/deploytoserver-view';
+
+describe('DeploytoserverView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
